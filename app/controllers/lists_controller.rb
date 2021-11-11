ba340ee2a6aa04f6b0c311120cdbde93043080f4
Emily Millard <emily_millard@live.co.uk>
2021-11-11 17:05:09 +0100

    feat: lists and bookmarks controllers with actions to create show and delete

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,32 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+    @bookmarks = Bookmark.where(list: @list)
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    end
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
